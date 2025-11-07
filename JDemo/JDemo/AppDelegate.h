4296414be238161f1e6dc10f5e33e544c5c36858
yanxuyao <289646686@qq.com>
2025-11-07 11:49:24 +0800

    [*] Just Demo 创建

---
+++ JDemo/JDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JDemo
+//
+//  Created by SuperC on 2025/11/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JDemo/JDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  JDemo
+//
+//  Created by SuperC on 2025/11/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
